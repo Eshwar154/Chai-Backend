70b3bccacebb6771c0b1387ebaa04fcd97b3ea78
Eshwar154 <eshwarpaygude@gmail.com>
2026-04-24 21:03:24 +0530

    add register controller & bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save",async function (next){
     if(!this.isModified("passsword"))return next();
-   this.passsword = bcrypt.hash(this.passsword,10)
+   this.passsword =  await bcrypt.hash(this.passsword,10)
    next()
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,21 @@
 import {Router} from "express"
 import {registeUser} from "../controllers/user.controller.js"
-
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
 
-router.route("/regsiter").post(registerUser)
+router.route("/regsiter").post(
+    upload.fields([
+        {
+                    name:"avatar",
+                    maxCount:1
+        },
+        {
+               name:"coverImage",
+               maxCount:1
+        }   
+    ]),
+    registerUser)
 
 export default router
