532b9f1a5c9c31320bc6d70fd9bf3073786183fb
Eshwar154 <eshwarpaygude@gmail.com>
2026-04-25 16:02:57 +0530

    debugging done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ import {upload} from "../middlewares/multer.middleware.js"
 const router = Router()
 
 
-router.route("/regsiter").post(
+router.route("/register").post(
     upload.fields([
         {
                     name:"avatar",
