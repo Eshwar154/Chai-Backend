7fa14945168d6dd69fd2462e29b10787f485888a
Eshwar154 <eshwarpaygude@gmail.com>
2026-04-25 16:35:25 +0530

    done with changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         trim:true,
 
     },
-    emial:{
+    email:{
         type:String,
         required:true,
         unique:true,
@@ -40,7 +40,7 @@ const userSchema = new Schema({
             ref:"Video"
         }
     ],
-    passsword:{
+    password:{
         type:String,
         required:[true,'Password is required']
 
